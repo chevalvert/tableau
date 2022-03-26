chevalvert/tableau

commit 04ae831e6a6f2b74bbd15b1a0d70b2ed4ec15041

Refactor codebase, implement Timeline, improve UI/UX

src/components/Item.js
@@ -1,40 +1,75 @@
 import Store from 'store'
 import { Component } from 'utils/jsx'
-import { writable } from 'utils/state'
+import { derived, writable } from 'utils/state'
+import { clamp, map } from 'missing-math'
 import classnames from 'classnames'
-import Icon from 'components/Icon'
+import hash from 'object-hash'
 
-import noop from 'utils/noop'
+import Button from 'components/Button'
+import Icon from 'components/Icon'
 
 export default class Item extends Component {
   beforeRender (props) {
+    this.handleData = this.handleData.bind(this)
+
     this.handleKeydown = this.handleKeydown.bind(this)
-    this.handleColorChange = this.handleColorChange.bind(this)
+    this.handleColor = this.handleColor.bind(this)
     this.handleFocus = this.handleFocus.bind(this)
     this.handleBlur = this.handleBlur.bind(this)
 
-    this.state = {
-      sortable: props.sortable === undefined ? true : props.sortable,
-      colorable: props.colorable === undefined ? true : props.colorable,
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.handleMouseMove = this.handleMouseMove.bind(this)
+    this.handleMouseUp = this.handleMouseUp.bind(this)
 
-      column: writable(props.column),
-      colors: writable(props.colors || []),
+    this.handleDelete = this.handleDelete.bind(this)
+
+    this.state = {
+      data: writable(props.data || {}),
 
       hasFocus: writable(false),
-      isHighlighted: writable(props.highlighted || false)
+      isResizing: writable(false),
+      isFiltered: undefined // See below
     }
+
+    this.state.isFiltered = derived(Store.filter, names => {
+      if (!names) return
+
+      // WARNING: potential memory leak
+      if (!this.state) return
+
+      const { colors } = this.state.data.get()
+      for (const hex of (colors || [])) {
+        const color = Store.colors.current[hex]
+        if (color && names.includes(color.toLowerCase())) return true
+      }
+    })
   }
 
   template (props, state) {
+    const { name, colors, index, timeline = {} } = state.data.get()
+
     return (
       <li
-        id={props.id}
+        id={index}
         class={classnames('item', props.class)}
         store-class-has-focus={this.state.hasFocus}
-        store-class-is-highlighted={this.state.isHighlighted}
-        style={props.style}
+        store-class-is-filtered={this.state.isFiltered}
+        store-class-is-resizing={this.state.isResizing}
+        style={`
+          --start: ${timeline.start};
+          --end: ${timeline.end};
+        `}
       >
-        {state.sortable && <Icon name='bars' class='item__handle' />}
+        {props.resizable && (
+          ['before', 'after'].map(side => (
+            <div
+              class='item__resize-handle'
+              data-side={side}
+              event-mousedown={this.handleMouseDown}
+            />
+          ))
+        )}
+        {props.sortable && <Icon name='bars' class='item__handle' />}
         <div
           contentEditable
           placeholder={props.placeholder}
@@ -44,45 +79,66 @@ export default class Item extends Component {
           event-focus={this.handleFocus}
           event-blur={this.handleBlur}
         >
-          {props.name}
+          {name}
         </div>
-        {state.colorable && (
+        {props.colorable && (
           <div class='item__colors'>
             {
               Array.from(new Set([
                 ...Object.keys(Store.colors.get()),
-                ...state.colors.get()
+                ...(colors || [])
               ])).map(color => (
                 <input
                   type='checkbox'
                   style={`--color: ${color}`}
-                  checked={state.colors.current.includes(color)}
-                  event-change={this.handleColorChange(color)}
+                  checked={colors && colors.includes(color)}
+                  event-change={this.handleColor(color)}
                 />
               ))
             }
           </div>
         )}
-        {props.children && <div class='item__buttons'>{props.children}</div>}
+        <div class='item__buttons'>
+          {props.children}
+          {props.deletable && <Button icon='trash' class='button--trash' event-click={this.handleDelete} />}
+        </div>
       </li>
     )
   }
 
-  get DOMIndex () {
-    return [...this.base.parentNode.children].indexOf(this.base)
+  afterMount () {
+    this.state.data.subscribe(this.handleData)
+    this.applyResize(this.state.data.current.timeline)
+
+    window.addEventListener('mousemove', this.handleMouseMove)
+    window.addEventListener('mouseup', this.handleMouseUp)
+    window.addEventListener('mouseleave', this.handleMouseUp)
   }
 
-  toJson () {
-    return {
-      name: this.refs.content.textContent,
-      column: this.state.column.get(),
-      colors: this.state.colors.get()
-    }
+  applyResize (timeline) {
+    if (!timeline) return
+    this.base.style.setProperty('--start', timeline.start)
+    this.base.style.setProperty('--end', timeline.end)
+    this.base.classList.toggle('is-small', timeline.end - timeline.start < 3)
+    this.base.classList.toggle('is-large', timeline.end - timeline.start > 10)
   }
 
-  // This is called by the parent component
-  handleSort (e) {
-    this.state.column.set(this.base.parentNode.dataset('index'))
+  handleData (data) {
+    // Do nothing if data is unchanged
+    if (hash(data) === hash(this.state.data.previous)) return
+
+    // Find and update the matching item in the Store
+    Store.items.update(items => {
+      if (data.deleted) delete items[data.index]
+
+      const item = items[data.index] || {}
+      for (const k in data) item[k] = data[k]
+
+      return items
+    }, true)
+
+    // Set the Store as dirty, which will trigger a database update
+    Store.dirty.set(true)
   }
 
   handleKeydown (e) {
@@ -90,30 +146,107 @@ export default class Item extends Component {
       e.preventDefault()
       this.refs.content.blur()
     }
-    ;(this.props['event-keydown'] || noop)(this)
   }
 
-  handleColorChange (color) {
-    return e => {
-      this.state.colors.update(colors => {
-        if (e.target.checked) colors.push(color)
-        else colors.splice(colors.indexOf(color), 1)
+  handleColor (color) {
+    return e => this.state.data.update(data => {
+      // Ensure data.colors exists
+      data.colors = data.colors || []
 
-        return Array.from(new Set([...colors]))
-      }, true)
+      // Insert or remove color based on input checked
+      if (e.target.checked) data.colors.push(color)
+      else data.colors.splice(data.colors.indexOf(color), 1)
 
-      ;(this.props['event-color'] || noop)(this)
-    }
+      // Ensure data.colors is deduped
+      data.colors = Array.from(new Set([...data.colors]))
+
+      return data
+    }, true)
   }
 
   handleFocus () {
     this.state.hasFocus.set(true)
-    ;(this.props['event-focus'] || noop)(this)
   }
 
   handleBlur () {
     this.refs.content.scrollLeft = 0
     this.state.hasFocus.set(false)
-    ;(this.props['event-blur'] || noop)(this)
+
+    // Update data.name
+    this.state.data.update(data => {
+      data.name = this.refs.content.textContent
+      return data
+    }, true)
+  }
+
+  handleMouseDown (e) {
+    this.state.isResizing.set(true)
+
+    this.refs.dragged = e.target
+    this.refs.dragged.start = e.pageX
+    this.refs.dragged.initialLeft = this.base.offsetLeft
+    this.refs.dragged.initialWidth = this.base.offsetWidth
+  }
+
+  handleMouseMove (e) {
+    if (!this.refs.dragged) return
+    const deltaX = e.pageX - this.refs.dragged.start
+
+    let left
+    let width
+
+    if (this.refs.dragged.dataset.side === 'before') {
+      left = this.refs.dragged.initialLeft + deltaX
+      width = e.shiftKey
+        ? this.refs.dragged.initialWidth
+        : (this.refs.dragged.initialWidth - deltaX)
+    }
+
+    if (this.refs.dragged.dataset.side === 'after') {
+      left = e.shiftKey
+        ? this.refs.dragged.initialLeft + deltaX
+        : this.refs.dragged.initialLeft
+      width = e.shiftKey
+        ? this.refs.dragged.initialWidth
+        : this.refs.dragged.initialWidth + deltaX
+    }
+
+    const containerWidth = this.base.parentNode.offsetWidth
+    // TODO: use the correct 0-52 weeks
+    const start = clamp(Math.round(map(left, 0, containerWidth, 0, 48)), 0, 48)
+    const end = clamp(Math.round(map(left + width, 0, containerWidth, 0, 48)), 0, 48)
+
+    if (start >= end) return
+    if (end <= start) return
+
+    this.applyResize({ start, end })
+    this.refs.dragged.range = [start, end]
+  }
+
+  handleMouseUp (e) {
+    if (!this.refs || !this.refs.dragged) return
+    this.state.isResizing.set(false)
+
+    const range = this.refs.dragged.range
+    delete this.refs.dragged
+
+    if (!range) return
+
+    this.state.data.update(data => {
+      if (!data.timeline) data.timeline = {}
+      data.timeline.start = range[0]
+      data.timeline.end = range[1]
+      return data
+    }, true)
+  }
+
+  handleDelete () {
+    this.state.data.update(data => ({ ...data, deleted: true }), true)
+  }
+
+  beforeDestroy () {
+    window.removeEventListener('mousemove', this.handleMouseMove)
+    window.removeEventListener('mouseup', this.handleMouseUp)
+    window.removeEventListener('mouseleave', this.handleMouseUp)
   }
 }

build/index.html
@@ -7,19 +7,18 @@
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
   <link rel="manifest" href="/site.webmanifest">
-  <meta name="theme-color" content="#ffffff">
+  <meta name="theme-color" content="#efefef">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
-  <link rel="stylesheet" href="bundle.css">
 </head>
-<body class="is-loading ">
+<body class="is-loading is-not-production">
   <script type="text/javascript">
     window.ENV = Object.assign({
       production:
         !!~window.location.hash.indexOf('#production') ||
-        (true && !~window.location.hash.indexOf('#dev')),
+        (false && !~window.location.hash.indexOf('#dev')),
       name: "tableau",
-      version: "1.8.1"
+      version: "1.8.2-dev"
     })
   </script>
   <script src="bundle.js"></script>

src/components/App.js
@@ -1,22 +1,32 @@
 import Store from 'store'
 import { Component } from 'utils/jsx'
+import { derived } from 'utils/state'
 
 import Colors from 'components/Colors'
 import Icon from 'components/Icon'
 import Items from 'components/Items'
 import Login from 'components/Login'
+import Timeline from 'components/Timeline'
+
+// Cache scroll so that app can render without jumps
+let _scrollTop = 0
 
 export default class App extends Component {
   beforeRender () {
     this.update = this.update.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
+    this.state = {
+      hasFilter: derived(Store.filter, names => names && names.length)
+    }
   }
 
-  template (props) {
+  template (props, state) {
     return (
       <main
         id='App'
         class='app'
         store-class-is-dirty={Store.dirty}
+        store-class-has-filter={state.hasFilter}
         store-class-is-authenticated={Store.authenticated}
       >
         <Icon name='loader' class='icon--dirty' />
@@ -25,24 +35,30 @@ export default class App extends Component {
     )
   }
 
-  get items () {
-    return (this.refs.items && this.refs.items.items) || []
-  }
-
-  get colors () {
-    return (this.refs.colors && this.refs.colors.colors) || {}
-  }
-
   afterMount () {
     this.update()
+
+    document.addEventListener('scroll', this.handleScroll)
+
     Store.items.subscribe(this.update)
+    Store.columns.subscribe(this.update)
     Store.colors.subscribe(this.update)
+    Store.filter.subscribe(this.update)
   }
 
   update () {
     if (this.refs.items) this.refs.items.destroy()
     if (this.refs.colors) this.refs.colors.destroy()
-    this.render(<Items ref={this.ref('items')} items={Store.items.get()} />, this.base)
+    if (this.refs.timeline) this.refs.timeline.destroy()
+
+    this.render(<Items ref={this.ref('items')} />, this.base)
     this.render(<Colors ref={this.ref('colors')} />, this.base)
+    this.render(<Timeline ref={this.ref('timeline')} />, this.base)
+
+    document.documentElement.scrollTop = _scrollTop
+  }
+
+  handleScroll (e) {
+    _scrollTop = document.documentElement.scrollTop
   }
 }

src/components/App.scss
@@ -1,8 +1,20 @@
 .app {
   margin: 0 auto;
 
+  display: flex;
+  flex-direction: column;
+
+
+  .items, .timeline {
+    scroll-snap-align: start end;
+    height: calc(100vh - 5rem);
+  }
+
+  .colors {}
+
   .icon--dirty {
     @include easing(120ms, opacity);
+    z-index: 2;
     position: fixed;
     right: 1rem;
     top: 2rem;
@@ -14,7 +26,7 @@
   &.is-dirty .icon--dirty { opacity: 1; }
 
   &.is-authenticated .login,
-  &:not(.is-authenticated) .items {
+  &:not(.is-authenticated) > *:not(.login) {
     display: none;
   }
 }

src/components/Button.scss
@@ -1,5 +1,4 @@
 .button {
-  @include no-select;
   @include reset-button;
   cursor: pointer;
   padding: 1rem 0.75rem;

src/components/Colors.js
@@ -1,39 +1,49 @@
 import Store from 'store'
 import { Component } from 'utils/jsx'
+import classnames from 'classnames'
+import cuid from 'cuid'
+import noop from 'utils/noop'
 
-import Item from 'components/Item'
-import Button from 'components/Button'
+import Icon from 'components/Icon'
 
 export default class Colors extends Component {
   beforeRender () {
     this.handleEdit = this.handleEdit.bind(this)
+    this.handlePreview = this.handlePreview.bind(this)
   }
 
   template (props) {
     return (
-      <section class='colors' store-class-has-highlighted={Store.highlighted}>
+      <section class='colors'>
         {
-          Object.entries(Store.colors.get() || {}).map(([color, name]) => {
-            const highlighted = Store.highlighted.current && Store.highlighted.current.match(name)
+          Object.entries(Store.colors.get() || {}).map(([hex, name]) => {
+            const id = cuid()
+            const selected = name && Store.filter.current && Store.filter.current.includes(name.toLowerCase())
             return (
-              <Item
-                style={`--color: black; --background: ${color}`}
-                colorable={false}
-                sortable={false}
-                deletable={false}
-                highlighted={highlighted}
-                data-color={color}
-                name={name}
-                event-blur={this.handleEdit}
-                ref={this.refArray('items')}
+              <form
+                class={classnames('color', { 'is-filtered': selected })}
+                style={`--color: black; --background: ${hex}`}
+                event-submit={this.handleEdit(hex)}
               >
-                {name && (
-                  <Button
-                    icon={highlighted ? 'hidden' : 'preview'}
-                    event-click={() => this.handlePreview(name)}
-                  />
-                )}
-              </Item>
+                <input
+                  type='text'
+                  value={name}
+                  class={classnames({ 'is-filtered': selected })}
+                  event-blur={this.handleEdit(hex)}
+                />
+                {name && [
+                  <input
+                    id={id}
+                    type='checkbox'
+                    checked={selected}
+                    event-change={this.handlePreview(name)}
+                  />,
+                  <label for={id}>
+                    <Icon name='preview' class='icon--preview' />
+                    <Icon name='hidden' class='icon--hidden' />
+                  </label>
+                ]}
+              </form>
             )
           })
         }
@@ -41,26 +51,31 @@ export default class Colors extends Component {
     )
   }
 
-  get colors () {
-    const colors = {}
+  handleEdit (hex) {
+    return e => {
+      e.preventDefault()
+      const input = e.target.tagName === 'FORM'
+        ? e.target.querySelector('input[type=text]')
+        : e.target
+      if (!input || !input.value) return
 
-    for (const item of this.refs.items || []) {
-      const color = item.props['data-color']
-      const { name } = item.toJson()
-      colors[color] = name
+      Store.colors.update(colors => {
+        colors[hex] = input.value
+        return colors
+      }, true)
+      Store.dirty.set(true)
     }
-
-    return colors
-  }
-
-  handleEdit (item) {
-    if (item.props.name === item.toJson().name) return
-    Store.dirty.set(true)
   }
 
   handlePreview (name) {
-    window.location.href = Store.highlighted.current && Store.highlighted.current.match(name)
-      ? window.location.origin
-      : window.location.origin + '?' + name
+    if (!name) return noop
+    name = name.toLowerCase()
+    return e => {
+      Store.filter.update((names = []) => {
+        if (e.target.checked) names.push(name)
+        else names.splice(names.indexOf(name), 1)
+        return Array.from(new Set([...names]))
+      }, true)
+    }
   }
 }

src/components/Colors.scss
@@ -1,37 +1,56 @@
 .colors {
-  z-index: 2;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
+  @include invisible-scroll;
   display: flex;
   padding: 1.2rem;
+  gap: 3px;
 
-  border-top: 1px dashed $color-border;
+  border-top: 1px solid $color-border;
+  border-bottom: 1px solid $color-border;
 
-  @include mobile {
+  form.color {
     position: relative;
-    flex-wrap: wrap;
-  }
-
-  .item {
     flex: 1;
-    height: 2rem;
-    font-size: 1.2rem;
-    font-variant: small-caps;
-    text-transform: lowercase;
-    margin: 3px;
 
     @include mobile {
-      flex: 33.33%;
+      min-width: 40%;
     }
 
-    .item__buttons .button {
-      margin-top: -1px;
-    }
-  }
+    input[type=text] {
+      @include reset-button;
+      @include item;
+      width: 100%;
+      height: 2rem;
+      font-size: 1.2rem;
+      font-variant: small-caps;
+      text-transform: lowercase;
 
-  &.has-highlighted .is-highlighted .item__buttons {
-    display: flex;
+      padding-right: 2rem;
+    }
+
+    input[type=checkbox] {
+      display: none;
+    }
+
+    label {
+      cursor: pointer;
+      position: absolute;
+      z-index: 1;
+      top: 1px;
+      right: 5px;
+
+      &::after {
+        @include inset(-5px);
+        content: '';
+      }
+    }
+
+    input:checked + label .icon.icon--preview,
+    input:not(:checked) + label .icon.icon--hidden {
+      display: none;
+    }
+
+    &:not(:hover):not(.is-filtered) label {
+      display: none;
+    }
   }
 }

src/components/Item.scss
@@ -1,14 +1,7 @@
 .item {
+  @include item;
+  position: relative;
   display: flex;
-
-  font-size: 1.5rem;
-  margin-bottom: 3px;
-  padding: 5px 7px;
-  height: 34px;
-  border-radius: 3px;
-
-  background: var(--background, black);
-  color: var(--color, white);
 
   .button, .icon {
     width: 1.2rem;
@@ -40,6 +33,28 @@
     visibility: hidden;
   }
 
+  &.sortable-chosen {
+    width: auto;
+  }
+
+  &__resize-handle {
+    z-index: 2;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+
+    // Increase hitbox
+    width: 1rem;
+    cursor: ew-resize;
+
+    @include mobile {
+      display: none;
+    }
+
+    &[data-side='before'] { left: -0.5rem; }
+    &[data-side='after'] { right: -0.25rem; }
+  }
+
   &__handle {
     cursor: move;
     margin-left: 0.5rem;
@@ -51,16 +66,7 @@
     color: black;
   }
 
-  .has-highlighted &:not(.is-highlighted):not(.item--add):not(.has-focus):not(:hover) {
-    outline: 1px solid black;
-    outline-offset: -1px;
-    background: transparent;
-    color: black;
-    opacity: 0.1;
-  }
-
   &__name {
-    @include no-select;
     cursor: text;
     flex: 1;
     align-self: center;
@@ -76,6 +82,14 @@
     &:focus {
       outline: 0;
       text-overflow: none;
+    }
+  }
+
+  &.is-resizing {
+    @include easing(120ms, transform);
+    transform: scale(1, 0.1);
+    * {
+      visibility: hidden;
     }
   }
 

commit 94a8a63fcb72f3070479d016467af77acf9b07f3

Fix regression introduced in 04ae831

src/components/Item.js
@@ -290,6 +290,7 @@ export default class Item extends Component {
   }
 
   handleDelete () {
+    if (!window.confirm(`Supprimer définitivement "${this.state.data.current.name}" ?`)) return
     this.state.data.update(data => ({ ...data, deleted: true }), true)
   }
 