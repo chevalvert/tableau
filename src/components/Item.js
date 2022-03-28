chevalvert/tableau

commit 3c38c25e660080f0951f8b90f691a2ec980a6ea9

Greatly improve timeline scroll UX

src/components/Item.js
@@ -101,38 +101,39 @@ export default class Item extends Component {
             />
           ))
         )}
-        {props.sortable && <Icon name='bars' class='item__handle' />}
-        <div
-          contentEditable
-          placeholder={props.placeholder}
-          class='item__name'
-          ref={this.ref('content')}
-          event-keydown={this.handleKeydown}
-          event-focus={this.handleFocus}
-          event-blur={this.handleBlur}
-        >
-          {name}
-        </div>
-        {props.colorable && (
-          <div class='item__colors'>
-            {
-              Array.from(new Set([
-                ...Object.keys(Store.colors.get()),
-                ...(colors || [])
-              ])).map(color => (
-                <input
-                  type='checkbox'
-                  style={`--color: ${color}`}
-                  checked={colors && colors.includes(color)}
-                  event-change={this.handleColor(color)}
-                />
-              ))
-            }
+        <div class='item__content'>
+          {props.sortable && <Icon name='bars' class='item__handle' />}
+          <div
+            placeholder={props.placeholder}
+            class='item__name'
+            ref={this.ref('content')}
+            event-keydown={this.handleKeydown}
+            event-focus={this.handleFocus}
+            event-blur={this.handleBlur}
+          >
+            {name}
           </div>
-        )}
-        <div class='item__buttons'>
-          {props.children}
-          {props.deletable && <Button icon='trash' class='button--trash' event-click={this.handleDelete} />}
+          {props.colorable && (
+            <div class='item__colors'>
+              {
+                Array.from(new Set([
+                  ...Object.keys(Store.colors.get()),
+                  ...(colors || [])
+                ])).map(color => (
+                  <input
+                    type='checkbox'
+                    style={`--color: ${color}`}
+                    checked={colors && colors.includes(color)}
+                    event-change={this.handleColor(color)}
+                  />
+                ))
+              }
+            </div>
+          )}
+          <div class='item__buttons'>
+            {props.children}
+            {props.deletable && <Button icon='trash' class='button--trash' event-click={this.handleDelete} />}
+          </div>
         </div>
       </li>
     )

src/components/Item.scss
@@ -3,6 +3,11 @@
   position: relative;
   display: flex;
 
+  &__content {
+    display: flex;
+    width: 100%;
+  }
+
   .button, .icon {
     width: 1.2rem;
     height: 1.2rem;
@@ -11,7 +16,9 @@
   }
 
   &__handle {
+    @include easing(120ms, opacity);
     margin-top: 6px;
+    .item:not(:hover) & { opacity: 0.6; }
   }
 
   &__buttons {

src/components/Items.scss
@@ -55,4 +55,10 @@
     @include invisible-scroll;
     flex: 1;
   }
+
+  .item:not(:hover) .item__colors {
+    @include tablet {
+      display: none;
+    }
+  }
 }

src/components/Timeline.js
@@ -22,6 +22,7 @@ const MONTHS = [
 const YEAR_PROGRESS = (new Date().getTime() - new Date(new Date().getFullYear(), 0, 1).getTime()) / (new Date(new Date().getFullYear() + 1, 0, 1).getTime() - new Date(new Date().getFullYear(), 0, 1).getTime())
 
 // Cache scroll so that app can render without jumps
+let _scrollTop = 0
 let _scrollLeft = 0
 
 export default class Timeline extends Component {
@@ -60,6 +61,18 @@ export default class Timeline extends Component {
                 ))
               }
             </div>
+            <div class='timeline__ticks'>
+              {
+                MONTHS.map(month => (
+                  <div class='timeline__month'>
+                    <div class='timeline__week' />
+                    <div class='timeline__week' />
+                    <div class='timeline__week' />
+                    <div class='timeline__week' />
+                  </div>
+                ))
+              }
+            </div>
             <ul
               class='timeline__items'
               ref={this.ref('timeline')}
@@ -68,7 +81,7 @@ export default class Timeline extends Component {
                 Store.items.current
                   .filter(item => item.timeline)
                   .sort((a, b) => a.timeline.index - b.timeline.index)
-                  .map(item => <Item data={item} sortable resizable colorable />)
+                  .map(item => <Item data={item} sortable resizable />)
               }
             </ul>
           </div>
@@ -80,7 +93,7 @@ export default class Timeline extends Component {
           {
             Store.items.current
               .filter(item => !item.timeline)
-              .map(item => <Item data={item} sortable colorable />)
+              .map(item => <Item data={item} sortable />)
           }
         </div>
       </section>
@@ -88,6 +101,7 @@ export default class Timeline extends Component {
   }
 
   afterMount () {
+    this.refs.scrollable.scrollTop = _scrollTop
     this.refs.scrollable.scrollLeft = _scrollLeft
 
     this.sortables = []
@@ -116,7 +130,7 @@ export default class Timeline extends Component {
       let index = 0
       for (const { id } of this.refs.timeline.querySelectorAll('.item')) {
         const item = find(id)
-        item.timeline = item.timeline || {}
+        item.timeline = item.timeline || { start: 48 * YEAR_PROGRESS, end: 48 * YEAR_PROGRESS + 4 }
         item.timeline.index = index++
       }
 
@@ -128,6 +142,7 @@ export default class Timeline extends Component {
   }
 
   handleScroll (e) {
+    _scrollTop = this.refs.scrollable.scrollTop
     _scrollLeft = this.refs.scrollable.scrollLeft
   }
 

src/components/Timeline.scss
@@ -31,28 +31,28 @@
   &__content {
     position: relative;
     width: min-content;
-    height: 100%;
+    min-height: 100%;
   }
 
   &__months {
-    pointer-events: none;
+    z-index: 1;
+    position: sticky;
+    top: 0;
     display: flex;
-    height: 100%;
     padding: 0 1.5rem;
+    height: 4.9rem;
+    background: $color-background;
   }
 
   &__month {
     display: flex;
-
     position: relative;
+
     border-left: 1px solid $color-border;
+    &:first-child { border: 0; }
+    &:first-child::before { margin-left: -1.5rem; }
 
-    &:first-child {
-      border: 0;
-      &::before { margin-left: -1.5rem; }
-    }
-
-    &::before {
+    &[data-month]::before {
       content: attr(data-month);
 
       position: absolute;
@@ -72,33 +72,33 @@
 
   &__week {
     flex: 1;
-    border-left: 1px dashed $color-border;
     min-width: 5rem;
+  }
 
-    &:first-child {
-      border-left: 0;
-    }
+  &__ticks {
+    @include inset(0);
+    pointer-events: none;
+    display: flex;
+    padding: 0 1.5rem;
+    bottom: -5rem;
+  }
+
+  &__ticks &__week {
+    border-left: 1px dashed $color-border;
+    &:first-child { border: 0; }
   }
 
   &__current {
     pointer-events: none;
     z-index: 2;
     position: absolute;
-    // top: 4.7rem;
     top: 0;
-    bottom: 0;
+    bottom: -5rem;
     border-left: 2px solid #ff6188;
     left: calc(100% * var(--start));
   }
 
   &__items {
-    @include invisible-scroll(y);
-    z-index: 1;
-    position: absolute;
-    top: 5rem;
-    left: 0;
-    bottom: 0;
-    width: 100%;
     padding: 0 1.5rem 1.5rem;
   }
 
@@ -114,5 +114,12 @@
     &.is-small .item__colors {
       display: none;
     }
+  }
+
+  &__items .item__content {
+    position: sticky;
+    left: 1rem;
+    padding-right: 1.5rem;
+    max-width: min-content;
   }
 }

src/index.scss
@@ -27,7 +27,7 @@ $color-background: #EFEFEF;
     outline: 1px solid $color-border;
     outline-offset: -1px;
 
-    [contenteditable], .button, .item__handle {
+    .item__content {
       opacity: 0.1;
     }
 

commit 4f7c01fd0dfddcdb9926ee06c5293df63923b26c

Fix regression introduced in 3c38c25

src/components/Item.js
@@ -104,6 +104,7 @@ export default class Item extends Component {
         <div class='item__content'>
           {props.sortable && <Icon name='bars' class='item__handle' />}
           <div
+            contentEditable
             placeholder={props.placeholder}
             class='item__name'
             ref={this.ref('content')}