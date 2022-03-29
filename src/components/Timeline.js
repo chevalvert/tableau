chevalvert/tableau

commit b3220406238e7acc8f534cad104784bba9b1394b

Use sortable to update timeline item range

src/components/Timeline.js
@@ -2,6 +2,7 @@ import Store from 'store'
 import { Component } from 'utils/jsx'
 import { Sortable } from 'sortablejs/modular/sortable.core.esm.js'
 import hash from 'object-hash'
+import { clamp, map } from 'missing-math'
 import Item from 'components/Item'
 
 const MONTHS = [
@@ -110,32 +111,43 @@ export default class Timeline extends Component {
       this.sortables.push(Sortable.create(ref, {
         group: 'timeline',
         handle: '.item__handle',
-        onSort: this.handleSort,
         axis: 'vertical',
         onStart: () => this.base.classList.add('has-sorting'),
-        onEnd: () => this.base.classList.remove('has-sorting')
+        onEnd: this.handleSort
       }))
     }
   }
 
   handleSort (e) {
+    this.base.classList.remove('has-sorting')
     if (!this.refs) return
+
     const previous = hash(Store.items.get())
 
     Store.items.update(items => {
       const find = id => items.find(item => item.index === +id)
+      const current = find(e.item.id)
 
       // Handle item dropped into the void
-      if (e.to === this.refs.void) {
-        const item = find(e.item.id)
-        if (item) delete item.timeline
+      if (e.to === this.refs.void && current) delete current.timeline
+
+      // Update item timeline range
+      if (current) {
+        if (!current.timeline) current.timeline = {}
+
+        const left = e.originalEvent.clientX + this.refs.scrollable.scrollLeft
+        const d = Math.floor(map(left, 0, e.item.parentNode.offsetWidth, 0, 48)) - (current.timeline.start || 0)
+
+        current.timeline.start = clamp((current.timeline.start || 0) + d, 0, 48)
+        current.timeline.end = current.timeline.end
+          ? clamp(current.timeline.end + d, 0, 48)
+          : current.timeline.start + 4
       }
 
-      // Update timeline indexes
+      // Update all items indexes
       let index = 0
       for (const { id } of this.refs.timeline.querySelectorAll('.item:not(.fake)')) {
         const item = find(id)
-        item.timeline = item.timeline || { start: Math.round(48 * YEAR_PROGRESS), end: 48 * YEAR_PROGRESS + 4 }
         item.timeline.index = index++
       }
 

src/components/Timeline.scss
@@ -133,6 +133,12 @@
       background: rgba(0, 0, 0, 0.3);
       border-radius: 3px;
     }
+
+    &.sortable-ghost {
+      width: 100%;
+      margin-left: 0;
+      transform: scale(1, 0.1) !important;
+    }
   }
 
   &__items .item__content {

commit ccff0bee6b9c1182c95f72de7f80d88e6b24dac0

Fix regression introduced in b322040

src/components/Timeline.js
@@ -128,9 +128,6 @@ export default class Timeline extends Component {
       const find = id => items.find(item => item.index === +id)
       const current = find(e.item.id)
 
-      // Handle item dropped into the void
-      if (e.to === this.refs.void && current) delete current.timeline
-
       // Update item timeline range
       if (current) {
         if (!current.timeline) current.timeline = {}
@@ -143,6 +140,9 @@ export default class Timeline extends Component {
           ? clamp(current.timeline.end + d, 0, 48)
           : current.timeline.start + 4
       }
+
+      // Handle item dropped into the void
+      if (e.to === this.refs.void && current) delete current.timeline
 
       // Update all items indexes
       let index = 0

src/components/Timeline.scss
@@ -133,12 +133,12 @@
       background: rgba(0, 0, 0, 0.3);
       border-radius: 3px;
     }
+  }
 
-    &.sortable-ghost {
-      width: 100%;
-      margin-left: 0;
-      transform: scale(1, 0.1) !important;
-    }
+  &__items .item.sortable-ghost {
+    width: 100%;
+    margin-left: 0;
+    transform: scale(1, 0.1) !important;
   }
 
   &__items .item__content {